dd9a53692eb921448dec858483437016fdf026dc
excing <courage.exc@gmail.com>
2022-12-12 00:52:57 +0800

    Update: restful api 编写完成

--- main/main.go
+++ main/main.go
@@ -8,15 +8,16 @@ import (
 	"net/http"
 	"os"
 
-	"community.threetenth.chatgpt/openai"
+	"community.threetenth.chatgpt/db"
+	"community.threetenth.chatgpt/restapi"
 	"community.threetenth.chatgpt/webapp"
-	"github.com/gin-contrib/sse"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 // Config is 服务启动配置文件
 type Config struct {
+	Pg    string
 	Port  int
 	Mode  log.Level
 	Log   string
@@ -40,7 +41,7 @@ func main() {
 	}
 
 	if config == nil {
-		config = &Config{30039, log.WarnLevel, "../logcat.log", true}
+		config = &Config{"postgres:123456@localhost:5432/chatgpt-community", 30039, log.WarnLevel, "../logcat.log", true}
 	}
 
 	log.SetLevel(config.Mode)
@@ -59,6 +60,8 @@ func main() {
 		log.Panicln("Failed to log to file, using default stderr", err)
 	}
 
+	db.OpenPostgreSQL(config.Pg, config.Debug)
+
 	if config.Debug {
 		gin.SetMode(gin.DebugMode)
 	} else {
@@ -75,8 +78,10 @@ func main() {
 	})
 	router.GET("/", web)
 	router.GET("/:pagename", web)
-	router.GET("/api/v1/session", updateChatGPTSession)
-	router.GET("/api/v1/conversation", getChatGPTConversation)
+	router.GET("/api/v1/session", restapi.UpdateChatGPTSession)
+	router.POST("/api/v1/conversation", restapi.PostChatGPTConversation)
+	router.GET("/api/v1/conversation", restapi.GetChatGPTConversation)
+	router.GET("/api/v1/message", restapi.GetChatGPTMessage)
 
 	router.Run(fmt.Sprint(":", config.Port))
 }
@@ -92,91 +97,4 @@ func web(c *gin.Context) {
 		return
 	}
 	tmpl.Execute(c.Writer, nil)
-}
-
-func getChatGPTConversation(c *gin.Context) {
-	// 获取 accessToken
-	accessToken := c.GetHeader("Authorization")
-	if accessToken == "" {
-		c.String(http.StatusUnauthorized, "Authorization header is required")
-		return
-	}
-
-	accept := c.GetHeader("accept")
-	if accept == "text/event-stream" {
-		getChatGPTConversationStream(c, accessToken)
-	} else {
-		getChatGPTConversationText(c, accessToken)
-	}
-}
-
-func getChatGPTConversationText(c *gin.Context, accessToken string) {
-	var json openai.ChatRequestJSON
-	if err := c.ShouldBindJSON(&json); err != nil {
-		// json 结构解析错误，返回错误
-		c.String(http.StatusBadRequest, err.Error())
-		return
-	}
-
-	// 调用 PostChatGPTText 函数，并返回结果
-	result, err := openai.PostChatGPTText(accessToken, json)
-	if err != nil {
-		c.String(http.StatusBadRequest, err.Error())
-		return
-	}
-	c.JSON(200, result)
-}
-
-func getChatGPTConversationStream(c *gin.Context, accessToken string) {
-	// 使用 json 结构获取参数
-	var chat openai.ChatRequestJSON
-	if err := c.ShouldBindJSON(&chat); err != nil {
-		c.String(http.StatusBadRequest, err.Error())
-		return
-	}
-
-	var err error
-	err = openai.PostChatGPTStream(accessToken, chat, func() {
-		// 回复支持 text/event-stream 格式
-		c.Header("Content-Type", "text/event-stream")
-		c.Header("Cache-Control", "no-cache")
-		c.Header("Connection", "keep-alive")
-		c.Header("Transfer-Encoding", "chunked")
-	}, func(msg *openai.ChatResponseJSON) (bool, error) {
-		err = sse.Encode(c.Writer, sse.Event{
-			Data: msg,
-		})
-		if err == nil {
-			c.Writer.Flush()
-		}
-
-		return !c.IsAborted(), nil
-	})
-	if err != nil {
-		c.String(http.StatusInternalServerError, err.Error())
-	}
-	// else {
-	// c.Status(http.StatusOK)
-	// }
-}
-
-func updateChatGPTSession(c *gin.Context) {
-	// 从请求的 header 中获取 sessionToken
-	sessionToken := c.Request.Header.Get("Authorization")
-	if sessionToken == "" {
-		// 如果 sessionToken 为空，返回 HTTP 400 错误
-		c.String(http.StatusUnauthorized, `Authorization is required. Please click "Application" - "Cookies" - "https://chat.openai.com" in the debugging tool of the ChatGPT page after login, and then copy the value of "__Secure-next-auth.session-token" inside , this value is the value of the current API Authorization.`)
-		return
-	}
-
-	// 调用 UpdateChatGPTSession 函数
-	token, err := openai.UpdateChatGPTSession(sessionToken)
-	if err != nil {
-		// 如果有错误，返回 HTTP 500 错误
-		c.String(http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	// 将 token 的值作为 HTTP 响应返回给客户端
-	c.JSON(http.StatusOK, &token)
 }
---
+++ restapi/restful_api.go
@@ -0,0 +1,233 @@
+package restapi
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"community.threetenth.chatgpt/db"
+	"community.threetenth.chatgpt/ent"
+	"community.threetenth.chatgpt/openai"
+	"github.com/gin-contrib/sse"
+	"github.com/gin-gonic/gin"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	// ContentTypeEventStream is text/event-stream content-type
+	ContentTypeEventStream = "text/event-stream"
+)
+
+var userTokenMap map[string]string
+
+func init() {
+	userTokenMap = make(map[string]string)
+}
+
+// GetChatGPTConversation 获取一个指定的会话
+func GetChatGPTConversation(c *gin.Context) {
+	getIDAndOkJSON(c, func(id string) (interface{}, error) {
+		return db.GetConversation(id)
+	})
+}
+
+// GetChatGPTMessage 获取一个指定的消息
+func GetChatGPTMessage(c *gin.Context) {
+	getIDAndOkJSON(c, func(id string) (interface{}, error) {
+		return db.GetMessage(id)
+	})
+}
+
+// PostChatGPTConversation 提交一个 ChatGPT 会话，并获取回复
+//
+// 支持 text/event-stream 流模式和文本模式
+func PostChatGPTConversation(c *gin.Context) {
+	// 获取 accessToken
+	accessToken := c.GetHeader("Authorization")
+	if accessToken == "" {
+		c.String(http.StatusUnauthorized, "Authorization header is required")
+		return
+	}
+
+	userID, ok := userTokenMap[accessToken]
+	if !ok {
+		c.String(http.StatusUnauthorized, "Authorization failed")
+		return
+	}
+
+	var message *ent.Message
+	var err error
+	if err = c.ShouldBindJSON(message); err != nil {
+		// json 结构解析错误，返回错误
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	message, err = db.SaveMessage(
+		message.ID,
+		message.Content,
+		message.ContentType,
+		message.Role,
+		message.ConversationID,
+		message.ParentMessageID,
+		userID,
+	)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"method": "restapi.PostChatGPTConversation",
+			"event":  "db.SaveMessage",
+		}).Info(err.Error())
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	chatRequestBody := openai.ChatRequestBody{
+		Action:         "next",
+		ConversationID: message.ConversationID,
+		Messages: []*openai.ChatMessage{
+			{
+				ID:   message.ID,
+				Role: "user",
+				Content: &openai.ChatContent{
+					ContentType: "text",
+					Parts:       []string{message.Content},
+				},
+			},
+		},
+		ParentMessageID: message.ParentMessageID,
+		Model:           "text-davinci-002-render",
+	}
+
+	var chatResponseBody *openai.ChatResponseBody
+
+	accept := c.GetHeader("accept")
+	if accept == ContentTypeEventStream {
+		chatResponseBody, err = getChatGPTConversationStream(c, accessToken, &chatRequestBody)
+	} else {
+		chatResponseBody, err = getChatGPTConversationText(c, accessToken, &chatRequestBody)
+	}
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"method": "restapi.PostChatGPTConversation",
+			"event":  accept,
+		}).Info(err.Error())
+		c.String(http.StatusServiceUnavailable, err.Error())
+		return
+	}
+
+	message, err = db.SaveMessage(
+		chatResponseBody.Message.ID,
+		chatResponseBody.Message.Content.Parts[0],
+		chatResponseBody.Message.Content.ContentType,
+		chatResponseBody.Message.Role,
+		chatResponseBody.ConversationID,
+		message.ID,
+		userID,
+	)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"method": "restapi.PostChatGPTConversation",
+			"event":  "db.SaveMessage",
+		}).Info(err.Error())
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if accept != ContentTypeEventStream {
+		c.JSON(http.StatusOK, message)
+	}
+}
+
+func getChatGPTConversationText(c *gin.Context, accessToken string, chatRequestBody *openai.ChatRequestBody) (*openai.ChatResponseBody, error) {
+	// 调用 PostChatGPTText 函数，并返回结果
+	return openai.PostChatGPTText(accessToken, chatRequestBody)
+}
+
+func getChatGPTConversationStream(c *gin.Context, accessToken string, chatRequestBody *openai.ChatRequestBody) (*openai.ChatResponseBody, error) {
+	var err error
+	return openai.PostChatGPTStream(accessToken, chatRequestBody, func() {
+		// 回复支持 text/event-stream 格式
+		c.Header("Content-Type", "text/event-stream")
+		c.Header("Cache-Control", "no-cache")
+		c.Header("Connection", "keep-alive")
+		c.Header("Transfer-Encoding", "chunked")
+	}, func(msg *openai.ChatResponseBody) (bool, error) {
+		err = sse.Encode(c.Writer, sse.Event{
+			Data: msg,
+		})
+		if err == nil {
+			c.Writer.Flush()
+		}
+
+		return !c.IsAborted(), nil
+	})
+}
+
+// UpdateChatGPTSession 更新 ChatGPT 用户的身份令牌
+func UpdateChatGPTSession(c *gin.Context) {
+	// 从请求的 header 中获取 sessionToken
+	sessionToken := c.Request.Header.Get("Authorization")
+	if sessionToken == "" {
+		// 如果 sessionToken 为空，返回 HTTP 400 错误
+		c.String(http.StatusUnauthorized, `Authorization is required. Please click "Application" - "Cookies" - "https://chat.openai.com" in the debugging tool of the ChatGPT page after login, and then copy the value of "__Secure-next-auth.session-token" inside , this value is the value of the current API Authorization.`)
+		return
+	}
+
+	// 调用 UpdateChatGPTSession 函数
+	token, err := openai.UpdateChatGPTSession(sessionToken)
+	if err != nil {
+		// 如果有错误，返回 HTTP 500 错误
+		log.WithFields(log.Fields{
+			"method": "restapi.UpdateChatGPTSession",
+			"event":  "openai.UpdateChatGPTSession",
+		}).Info(err.Error())
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	err = db.SaveUser(
+		token.User.ID,
+		token.User.Name,
+		token.User.Email,
+		token.User.Image,
+		token.User.Groups,
+		token.User.Features,
+	)
+
+	if err != nil {
+		bs, _ := json.Marshal(&token.User)
+		log.WithFields(log.Fields{
+			"api":   "restapi.UpdateChatGPTSession",
+			"event": "db.SaveUser",
+			"data":  string(bs),
+		}).Info(err.Error())
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	userTokenMap[token.AccessToken] = token.User.ID
+
+	// 将 token 的值作为 HTTP 响应返回给客户端
+	c.JSON(http.StatusOK, &token)
+}
+
+func getIDAndOkJSON(c *gin.Context, handle func(id string) (interface{}, error)) {
+	id := c.Query("id")
+	if id == "" {
+		c.String(http.StatusUnauthorized, "id can't empty")
+		return
+	}
+	data, err := handle(id)
+	if err != nil {
+		if ent.IsNotFound(err) {
+			c.String(http.StatusNotFound, err.Error())
+		} else {
+			c.String(http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	c.JSON(http.StatusOK, &data)
+}
